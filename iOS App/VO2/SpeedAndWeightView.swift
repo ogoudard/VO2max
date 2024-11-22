081579bff35216f8479f3ab2b8b91ee3ee41e408
Olivier Goudard <goudard@esrf.fr>
2024-11-22 11:30:04 +0100

    [iOS] Improve interval creation process

--- iOS App/VO2/SpeedAndWeightView.swift
+++ iOS App/VO2/SpeedAndWeightView.swift
@@ -6,38 +6,46 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct SpeedAndWeightView: View {
     @State private var estimatedSpeed: String = "" // Stocker la vitesse maximale estimée
     @State private var weight: String = "" // Stocker le poids
     @Environment(\.dismiss) var dismiss // Pour revenir à la vue précédente
+    @State private var speechSynthesizer = AVSpeechSynthesizer()
     
+    func speak(text: String) {
+         let utterance = AVSpeechUtterance(string: text)
+         utterance.voice = AVSpeechSynthesisVoice(language: "en-US") // Choisissez la langue que vous préférez
+         speechSynthesizer.speak(utterance)
+     }
+
     var body: some View {
         VStack {
-            Text("Entrez la vitesse maximale estimée et votre poids")
+            Text("Enter your max speed estimation and your weight")
                 .font(.title2)
                 .padding()
             
             // Champ pour la vitesse maximale
-            TextField("Vitesse maximale (km/h)", text: $estimatedSpeed)
+            TextField("Max speed (km/h)", text: $estimatedSpeed)
                 .keyboardType(.decimalPad) // Permet la saisie de nombres
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal)
             
             // Champ pour le poids
-            TextField("Poids (kg)", text: $weight)
+            TextField("Weight (kg)", text: $weight)
                 .keyboardType(.decimalPad) // Permet la saisie de nombres
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal)
             
             // Bouton pour valider les données
-            Button("Valider") {
+            Button("OK") {
                 // Action lorsque l'utilisateur valide les entrées
                 print("Vitesse estimée : \(estimatedSpeed) km/h")
                 print("Poids : \(weight) kg")
-                
+                speak(text: "Intervals created. Get ready to suffer!")
                 // Vous pouvez ici enregistrer les valeurs ou les utiliser dans votre logique
                 dismiss() // Revenir à la page principale
             }
@@ -48,7 +56,7 @@ struct SpeedAndWeightView: View {
             .padding(.top)
             
             // Bouton pour annuler et revenir
-            Button("Annuler") {
+            Button("Dismmis") {
                 dismiss() // Revenir sans enregistrer
             }
             .padding()
@@ -61,4 +69,6 @@ struct SpeedAndWeightView: View {
         .navigationTitle("Détails du Test")
         .navigationBarTitleDisplayMode(.inline)
     }
+    
+
 }
